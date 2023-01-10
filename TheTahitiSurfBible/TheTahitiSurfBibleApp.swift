fe62b7aff7ee91c324ef860e96bed141cee70996
irJERAD <jsacosta1103@gmail.com>
2023-01-09 17:22:33 -1000

    Initial Commit

---
+++ TheTahitiSurfBible/TheTahitiSurfBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTahitiSurfBibleApp.swift
+//  TheTahitiSurfBible
+//
+//  Created by Jerad Acosta on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TheTahitiSurfBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
